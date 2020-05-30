876e088c8a94e4c8fb36938dfb54980f0b3bf4d1
Sid Haniff <syed_haniff@hotmail.com>
2020-05-30 12:29:42 +0100

    Add api for get messages and add message

--- src/main/java/xyz/dsoc/labs/fake/symphony/api/FakeSymphonyApi.java
+++ src/main/java/xyz/dsoc/labs/fake/symphony/api/FakeSymphonyApi.java
@@ -1,7 +1,61 @@
 package xyz.dsoc.labs.fake.symphony.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import xyz.dsoc.labs.fake.symphony.domain.Message;
+import xyz.dsoc.labs.fake.symphony.domain.Stream;
+import xyz.dsoc.labs.fake.symphony.service.BadMessageException;
+import xyz.dsoc.labs.fake.symphony.service.MessageService;
+import xyz.dsoc.labs.fake.symphony.service.NoSuchStreamException;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 /**
+ * This is meant to be a mock service for https://developers.symphony.com/restapi/reference#messages-v4
+ *
  * @author sih
  */
+@RestController
+@RequestMapping(value = "/streams")
 public class FakeSymphonyApi {
+
+    @Autowired
+    private MessageService messageService;
+
+    @GetMapping(path="/{streamId}/messages", produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Message>> getMessages(@PathVariable String streamId) throws NoSuchStreamException {
+
+        List<Message> messages = messageService.getMessages(streamId);
+
+        return new ResponseEntity<>(messages, HttpStatus.OK);
+    }
+
+    @PostMapping(path="/{streamId}/messages", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Message>> addMessage
+            (@PathVariable String streamId,
+             @RequestBody Message message)
+            throws BadMessageException, NoSuchStreamException {
+
+        Stream stream = messageService.addMessageToStream(streamId, message);
+
+        List<Message> messages = stream.getMessages();
+
+        return new ResponseEntity<>(messages, HttpStatus.CREATED);
+    }
+
+    @ExceptionHandler(NoSuchStreamException.class)
+    public void noStreamException(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @ExceptionHandler(BadMessageException.class)
+    public void badMessageException(HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    }
+
 }
---
+++ src/test/java/xyz/dsoc/labs/fake/symphony/api/FakeSymphonyApiTest.java
@@ -0,0 +1,122 @@
+package xyz.dsoc.labs.fake.symphony.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import xyz.dsoc.labs.fake.symphony.domain.Message;
+import xyz.dsoc.labs.fake.symphony.domain.Stream;
+import xyz.dsoc.labs.fake.symphony.service.MessageService;
+import xyz.dsoc.labs.fake.symphony.service.NoSuchStreamException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author sih
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@EnableWebMvc
+@AutoConfigureMockMvc
+public class FakeSymphonyApiTest {
+
+    private List<Message> messages;
+    private String mJson;
+    private Message m;
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Before
+    public void setUp() throws Exception {
+
+        messages = new ArrayList<>();
+
+        m = new Message();
+        m.setTimestamp(1234L);
+        m.setMessageId("1234");
+        m.setContent("Hello");
+
+        messages.add(m);
+
+        mJson  = mapper.writeValueAsString(m);
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private MessageService messageService;
+
+
+    @Test
+    public void whenNoStreamForIdGetMessagesShouldReturnNotFound() throws Exception {
+
+        when(messageService.getMessages("123")).thenThrow(new NoSuchStreamException("no stream"));
+        mockMvc
+                .perform(get("/streams/123/messages"))
+                .andExpect(status().isNotFound())
+        ;
+
+    }
+
+    @Test
+    public void whenMessagesInStreamGetMessagesShouldReturnOk() throws Exception {
+
+        when(messageService.getMessages("123")).thenReturn(messages);
+        mockMvc
+                .perform(get("/streams/123/messages"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].message", is("Hello")))
+        ;
+
+    }
+
+    @Test
+    public void whenNoStreamForIdAddMessageShouldReturnNotFound() throws Exception {
+        when(messageService.addMessageToStream("123", m)).thenThrow(new NoSuchStreamException("no stream"));
+        mockMvc
+                .perform(post("/streams/123/messages")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mJson)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isNotFound())
+        ;
+    }
+
+    @Test
+    public void whenValidMessageProvidedToAddMessageShouldReturnOk() throws Exception {
+
+        Stream s = new Stream();
+        s.addMessage(m);
+
+        when(messageService.addMessageToStream("123", m)).thenReturn(s);
+
+
+        mockMvc
+                .perform(post("/streams/123/messages")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mJson)
+                        .accept(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isCreated())
+        ;
+    }
+
+}
